b9510d4dabede435f49644e13cc06b031bd15914
Mifa43 <mifa43kotez@gmail.com>
2022-05-22 08:17:33 +0200

    url za home page\

--- main/urls.py
+++ main/urls.py
@@ -13,9 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from re import I
 from django.contrib import admin
 from django.urls import path
+from blog.views import HomeView
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/', HomeView.as_view()),
+
+
 ]
