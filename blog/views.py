50c1c6d627f762c2adad9e85efcb5c07cdf90d9a
Mifa43 <mifa43kotez@gmail.com>
2022-05-22 15:44:34 +0200

    nova klasa koja ce prikazivati post

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, CreateView
 
 # Create your views here.
 
 class HomeView(TemplateView):
-    template_name = "main/homepage.html"
+    template_name = "main/home.html"
+
+class BlogView(TemplateView):
+    template_name = "main/blog.html"
+
+class NewPost(CreateView):
+    pass
