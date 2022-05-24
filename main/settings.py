c24c81833f2e996c74b7cd8b294fbd24a8573336
Mifa43 <mifa43kotez@gmail.com>
2022-05-24 22:50:46 +0200

    dodat media folder za cuvanje slike, i dodat je redirekt za login logout

--- main/settings.py
+++ main/settings.py
@@ -129,6 +129,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = "/media/"
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATICFILES_DIRS = [
         os.path.join(BASE_DIR, 'static')
         ]
@@ -137,5 +140,7 @@ STATICFILES_DIRS = [
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
+LOGIN_REDIRECT_URL = "/blog"
+LOGOUT_REDIRECT_URL = "/home"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
