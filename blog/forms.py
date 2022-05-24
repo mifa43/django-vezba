d0bfafbbac14bf4f3da832fb9a677caff8360bdb
Mifa43 <mifa43kotez@gmail.com>
2022-05-24 22:48:05 +0200

    izbacena meta jer nije trebala da stoji tu i blokirala je kod

--- blog/forms.py
+++ blog/forms.py
@@ -3,7 +3,7 @@ from .models import Post
 from django.forms import ModelForm
 
 class PostForm(ModelForm):
-    class Meta:
-        model = Post
-        fields = ["title", "content", "created_on", "photo", "author"]
+    
+    model = Post
+    fields = ["title", "content", "created_on", "photo", "author"]
 
