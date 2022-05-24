2e25e2ee954c748aa513590f76e3fdee6327c5b2
Mifa43 <mifa43kotez@gmail.com>
2022-05-24 22:49:18 +0200

    izmene u blogview izbacen je stari pogled, sada se upisuje autor posta

--- blog/views.py
+++ blog/views.py
@@ -1,20 +1,24 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, CreateView
-from .forms import PostForm
+from django.views.generic import TemplateView, CreateView, ListView
+from .models import Post
 
 class HomeView(TemplateView):
     template_name = "main/home.html"
 
-class BlogView(TemplateView):
+class BlogView(ListView):
+    model = Post
+    context_object_name = "post"
     template_name = "main/blog.html"
 
 class NewPost(CreateView):
-    model = PostForm    # db
-    fields = ("title", "content", "photo", "author") 
+    model = Post   # db
+    fields = ("title", "content", "photo") 
     template_name = "main/postBlog.html"
     context_object_name = "form" 
     success_url = "/post-blog"  
     def form_valid(self, form) -> bool:
-  
+          
         form.instance.user = self.request.user 
-        return super(NewPost, self).form_valid(form) 
+        form.instance.author = self.request.user 
+
+        return super(NewPost, self).form_valid(form)  
